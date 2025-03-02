3a0416173e26fb275b83a8e70e855adc15462db1
lmz760923 <15919731630@163.com>
2025-03-02 22:18:44 +0800

    20250302

--- controller/api.js
+++ controller/api.js
@@ -1,17 +1,22 @@
 
 const service=require('../service/api');
 const apicontroller={
-    async genAddressList(req, res) {
-        const token=null;
-        let result=await service.genAddressList(1);
-        res.json(result);
-       },
-    async genAddress(req, res) {
+    
+    genAddress(req, res) {
         const id=req.body.id;
         console.log('payload:',req.auth);
-        let result=await service.genAddress(id);
-        res.json(result);
-      }
+        service.genAddress(id).then(result=>{res.json(result);})
+        
+      },
+
+      genAddressList(req, res) {
+        
+            service.genAddressList(1).then(result=>{
+                res.json(result)});
+            
+ 
+        
+       },
 
 };
 module.exports=apicontroller
