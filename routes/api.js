3f2076fcdfe11f7ec124c11cc21e8f99b314e010
lmz760923 <15919731630@163.com>
2025-03-02 21:54:30 +0800

    20250302

--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var db=require('../dao/db');
-const { resolveInclude } = require('ejs');
-
-function genAddress(id) {
-  return new Promise((resolve,reject)=>{
-    db.query('select * from user_address where id=?',[id],(err,results)=>{
-      if (err) {reject(err);}
-      if (results.length==0){resolve({})}
-      else resolve(results[0]);
-    });
-  });
-
-}
-
-function genAddressList(uid) {
-  return new Promise((resolve,reject)=>{
-    db.query('select * from user_address where uid=?',[uid],(err,results)=>{
-      if (err) {reject(err);}
-      
-      else resolve(results);
-    });
-  });
-}
+const express = require('express');
+const router = express.Router();
+const apicontroller=require('../controller/api.js');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json({code:0,message:'ok'});
 });
 
-router.post('/genAddress', async function(req, res) {
-  const id=req.body.id;
-  console.log('payload:',req.auth);
-  let result=await genAddress(id);
-  res.json(result);
-});
+router.post('/genAddress', apicontroller.genAddress);
 
-router.post('/genAddressList',async function(req, res) {
-   const token=null;
-   let result=await genAddressList(1);
-   res.json(result);
-  });
+router.post('/genAddressList',apicontroller.genAddressList);
 
 
 module.exports = router;
